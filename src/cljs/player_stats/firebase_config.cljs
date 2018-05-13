7857ee604854993e862da36be0f67bcd1e09c002
Matyas Korom <korommatyi@gmail.com>
2018-05-13 16:35:52 +0200

    Add firebase config

---
+++ src/cljs/player_stats/firebase_config.cljs
@@ -0,0 +1,8 @@
+(ns player-stats.firebase-config)
+
+(def config #js {:apiKey "AIzaSyC3J1d0aIsil-MVtWbDNzVVGXO0tPrnQgw"
+                 :authDomain "c99-foci-stats.firebaseapp.com"
+                 :databaseURL "https://c99-foci-stats.firebaseio.com"
+                 :projectId "c99-foci-stats"
+                 :storageBucket "c99-foci-stats.appspot.com"
+                 :messagingSenderId "439716566573"})
